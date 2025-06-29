9e18c3cc6a1e628929b0cf8c14c454f33593b927
Poya Faraji <poya.faraji@outlook.com>
2025-06-29 17:26:37 +0330

    Export Prisma
    Simple middle wares for server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 app.listen(port, (req, res) => {
   console.log(`Application is listening on port ${port}`);
 });
